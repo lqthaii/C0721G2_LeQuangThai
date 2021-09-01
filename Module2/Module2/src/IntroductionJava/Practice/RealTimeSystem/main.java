4859eae9ac8e7ab57c77b2a5cc1d9c9855ee3fb8
quangthaile <88078035+quangthaile@users.noreply.github.com>
2021-09-01 14:24:50 +0700

    update git

---
+++ Module2/Module2/src/IntroductionJava/Practice/RealTimeSystem/main.java
@@ -0,0 +1,10 @@
+package IntroductionJava.Practice.RealTimeSystem;
+
+import java.util.Date;
+
+public class main {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is:"+now);
+    }
+}
