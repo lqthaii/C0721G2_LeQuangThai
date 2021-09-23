9ebf3bf7aef03640775e783fa4e7106128293bac
quangthaile <88078035+quangthaile@users.noreply.github.com>
2021-09-23 17:17:31 +0700

    done

---
+++ Module2/Module2/src/_15_io_text_file/exercise/read_file/QuocGia.java
@@ -0,0 +1,49 @@
+package _15_io_text_file.exercise.read_file;
+
+public class QuocGia {
+    private int id;
+    private String code;
+    private String name;
+
+    public QuocGia() {
+    }
+
+    public QuocGia(int id, String code, String name) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "QuocGia{" +
+                "id=" + id +
+                ", code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- Module2/Module2/src/_15_io_text_file/exercise/read_file/ReadFile.java
+++ Module2/Module2/src/_15_io_text_file/exercise/read_file/ReadFile.java
@@ -1,10 +1,38 @@
 package _15_io_text_file.exercise.read_file;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.TreeMap;
 
 public class ReadFile {
-    public TreeMap readFile(String patch){
-        TreeMap<String,String> value = new TreeMap();
+    public TreeMap readFile(String patch) {
+        TreeMap<String, QuocGia> value = new TreeMap();
+        String str ="";
+        String [] arr = new String[2];
+        File file = new File(patch);
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while((str = bufferedReader.readLine())!=null){
+             arr = str.split(",");
+             QuocGia quocGia = new QuocGia(Integer.parseInt(arr[0]),arr[1],arr[2]);
+             value.put(arr[1],quocGia);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Lỗi đọc file");
+        }
         return value;
+    }
+
+    public static void main(String[] args) {
+        ReadFile readFile = new ReadFile();
+        TreeMap<String, QuocGia> value = new TreeMap();
+        value = readFile.readFile("src\\_15_io_text_file\\exercise\\read_file\\QuocGia.csv");
+        for(String quocGia : value.keySet()){
+            System.out.println(value.get(quocGia));
+        }
     }
 }
